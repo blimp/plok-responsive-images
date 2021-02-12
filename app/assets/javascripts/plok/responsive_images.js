af23b059a34bff52fa985ec03f2899d56bb85483
davelens <github@davelens.be>
2021-02-12 16:33:25 +0100

    Add the W3C polyfill for IntersectionObserver, and our homebrew plok.scrolled_to.

--- app/assets/javascripts/plok/responsive_images.js
+++ app/assets/javascripts/plok/responsive_images.js
@@ -1,4 +1,6 @@
+//= require w3c/polyfills/intersection-observer
 //= require bs-breakpoints/index
 //= require bs-breakpoints/polyfill
+//= require plok/scrolled_to
 //= require plok/breakpoint
 //= require plok/responsive-images
---
+++ app/assets/javascripts/plok/scrolled_to.js
@@ -0,0 +1,33 @@
+var plok = plok || {}
+plok.scrolled_to = {
+  observer: null,
+
+  init: function(selector, process, callback) {
+    var bind_observers_with_callback = function(entries){
+      scrolled_to.process_observations(entries, process, callback);
+    };
+
+    this.observer = new IntersectionObserver(bind_observers_with_callback, { rootMargin: "5% 0%" });
+    this.observe(selector, this.observer);
+  },
+
+  observe: function(selector) {
+    this.query(selector).forEach(function(item){
+      scrolled_to.observer.observe(item);
+    });
+  },
+
+  process_observations: function(entries, process, callback) {
+    entries.forEach(function(entry) {
+      if(!entry.isIntersecting) return;
+      process(entry);
+      scrolled_to.observer.unobserve(entry.target);
+    });
+
+    if(typeof callback == 'function') callback(entries);
+  },
+
+  query: function(selector) {
+    return Array.from(document.querySelectorAll(selector));
+  }
+};
