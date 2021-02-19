e6cf4871a2bc2a80f2feb3302db018a2c4e3ba9a
davelens <github@davelens.be>
2021-02-19 11:35:07 +0100

    Expand upon the Image object's tests, and work out an unnecessary Rails dependency.

--- lib/plok/responsive_images/image.rb
+++ lib/plok/responsive_images/image.rb
@@ -29,7 +29,7 @@ module Plok
 
       def html(lazy: false, html: {})
         data_attributes = { responsive: interchange }
-        html.reverse_merge!(data: data_attributes)
+        html = html.merge(data: data_attributes)
         @view_context.send(tag_method(lazy: lazy), default_version, html)
       end
     end
--- spec/lib/plok/responsive_images/image_spec.rb
+++ spec/lib/plok/responsive_images/image_spec.rb
@@ -1,6 +1,46 @@
 RSpec.describe Plok::ResponsiveImages::Image do
   let(:view) { double(:view_context) }
 
+  describe '#html' do
+    let(:attrs) do
+      {
+        data: {
+          responsive: '[small-image.jpg, (small)], [medium-image.jpg, (medium)]'
+        }
+      }
+    end
+
+    it 'returns an image tag with a default image version' do
+      versions = {
+        small: 'small-image.jpg',
+        medium: 'medium-image.jpg'
+      }
+
+      expect(view).to receive(:image_tag).with('medium-image.jpg', attrs)
+      described_class.new(view_context: view, versions: versions).html
+    end
+
+    it 'returns a lazy image tag with a default image version' do
+      versions = {
+        small: 'small-image.jpg',
+        medium: 'medium-image.jpg'
+      }
+
+      expect(view).to receive(:lazy_image_tag).with('medium-image.jpg', attrs)
+      described_class.new(view_context: view, versions: versions).html(lazy: true)
+    end
+  end
+
+  describe '#tag_method' do
+    it 'returns "lazy_image_tag" when lazy' do
+      expect(subject.tag_method(lazy: true)).to eq :lazy_image_tag
+    end
+
+    it 'defaults to "image_tag"' do
+      expect(subject.tag_method).to eq :image_tag
+    end
+  end
+
   describe '#interchange' do
     it 'returns "" by default' do
       expect(subject.interchange).to eq ''
@@ -15,6 +55,43 @@ RSpec.describe Plok::ResponsiveImages::Image do
       subject = described_class.new(view_context: view, versions: versions)
       expected = '[small-image.jpg, (small)], [medium-image.jpg, (medium)]'
       expect(subject.interchange).to eq expected
+    end
+  end
+
+  describe '#default_version' do
+    it 'prioritizes large version, when present' do
+      versions = {
+        small: 'small-image.jpg',
+        medium: 'medium-image.jpg',
+        large: 'large-image.jpg'
+      }
+
+      subject = described_class.new(view_context: view, versions: versions)
+      expect(subject.default_version).to eq 'large-image.jpg'
+    end
+
+    it 'prioritizes medium version, when present' do
+      versions = {
+        small: 'small-image.jpg',
+        medium: 'medium-image.jpg'
+      }
+
+      subject = described_class.new(view_context: view, versions: versions)
+      expect(subject.default_version).to eq 'medium-image.jpg'
+    end
+
+    it 'falls back to small version, when present' do
+      versions = {
+        small: 'small-image.jpg'
+      }
+
+      subject = described_class.new(view_context: view, versions: versions)
+      expect(subject.default_version).to eq 'small-image.jpg'
+    end
+
+    it 'defaults to nil' do
+      subject = described_class.new(view_context: view, versions: {})
+      expect(subject.default_version).to be nil
     end
   end
 end
